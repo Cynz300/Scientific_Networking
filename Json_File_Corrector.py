ab8e5941789544775dd0a56b07cc0728aecca209
Ilya G <ilyagetsin@gmail.com>
2021-06-11 00:03:12 -0400

    Update Json_File_Corrector.py

--- Json_File_Corrector.py
+++ Json_File_Corrector.py
@@ -1,30 +1,21 @@
 import json
-from tqdm import tqdm # google this
-# journals = []
+from tqdm import tqdm 
 idx = 0
 
 new = open("test_data.json", "w")
 
 with open('dblpv13.json',"r") as fp:
      for line in tqdm(fp):
-        # if idx > 10000: 
+        # if idx > 10000: #For Debugging if needed
         #     break
         try:
-            if len(line) < 3:
+            #This is somewhat arbitrary but the goal is to avoid lines with negligible information. I've found 3 is a sufficient enough parser
+            if len(line) < 3: 
                 continue
             strip_line = line.strip(',')
-#             hello = json.loads(strip_line)
-#             print(str(hello))
-            # journals.append(json.loads(strip_line))
             new.write(strip_line)
-            idx += 1
+#             idx += 1 #For Debugging if needed
         except:
             print('fail')
-#             try:
-#                 strip_line = line.strip(',')
-#                 # journals.append(json.loads(strip_line))
-# #                 new.write(strip_line + "\n")
-#                 idx += 1
-#             except:
-            idx += 1
+#             idx += 1 #For Debugging if needed
             pass
